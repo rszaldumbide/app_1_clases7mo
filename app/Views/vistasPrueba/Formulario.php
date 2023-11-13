2c6405d00aa96f8e29b6af0549832c9001acd52f
Ricardo Zaldumbide <rszaldumbide@pucesi.edu.ec>
2023-11-13 18:10:50 -0500

    required

--- app/Views/vistasPrueba/Formulario.php
+++ app/Views/vistasPrueba/Formulario.php
@@ -16,15 +16,15 @@
                             <form action="<?php echo base_url() . "index.php/envioDatosPrueba" ?>" method="GET" enctype="multipart/form-data">
                                 <div class="input-group my-3">
                                     <span class="input-group-text">Nombre:</span>
-                                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ingrese su nombre">
+                                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ingrese su nombre" required>
                                 </div>
                                 <div class="input-group my-3">
                                     <span class="input-group-text">Cedula:</span>
-                                    <input type="text" maxlength="10" onkeyup="verificar()" onkeypress="return ingresoNumeros(event);" class="form-control" name="cedula" id="cedula" placeholder="Ingrese su número de cédula">
+                                    <input required type="text" maxlength="10" onkeyup="verificar()" onkeypress="return ingresoNumeros(event);" class="form-control" name="cedula" id="cedula" placeholder="Ingrese su número de cédula">
                                 </div>
                                 <div class="input-group my-3">
                                     <span class="input-group-text">Fecha:</span>
-                                    <input type="date" class="form-control" name="fecha" id="fecha">
+                                    <input type="date" class="form-control" name="fecha" id="fecha" required>
                                 </div>
                                 <div class="text-center my-3">
                                     <input type="submit" id="btnEnviar" style="width: 50%; display: none;" class="btn btn-primary" value="Enviar">
